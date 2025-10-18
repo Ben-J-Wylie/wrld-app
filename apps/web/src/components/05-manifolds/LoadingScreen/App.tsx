c59c2591d896b52fe5a521c0115658f110c952c4
Ben-J-Wylie <ben.j.wylie@gmail.com>
2025-10-18 14:36:29 -0700

    Fonts from main.css

---
+++ apps/web/src/components/05-manifolds/LoadingScreen/App.tsx
@@ -0,0 +1,12 @@
+// @ts-nocheck
+
+import React from "react";
+import LoadingScreen from "./components/05-manifolds/LoadingScreen/LoadingScreen";
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <LoadingScreen />
+    </div>
+  );
+}
