81ad6238ae3eb4c93d998e6aa458fc25e2381099
Ben-J-Wylie <ben.j.wylie@gmail.com>
2025-12-05 15:48:25 -0800

    App tsx for fake shadows

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -10,34 +10,3 @@ import { AppScene } from "./components/Elements/Streaming/Audio/AppScene";
 export default function App() {
   return <AppScene />;
 }
-
-// App.tsx
-// import React from "react";
-// import { Canvas } from "@react-three/fiber";
-// import { DemoScene } from "./components/CoreScene/Shadows/AttemptOne/DemoScene";
-// import { FakeShadowProvider } from "./components/CoreScene/Shadows/AttemptOne/FakeShadowContext";
-
-// export default function App() {
-//   return (
-//     <FakeShadowProvider>
-//       <DemoScene />
-//     </FakeShadowProvider>
-//   );
-// }
-
-// import React from "react";
-// import { DemoScene } from "./components/CoreScene/Shadows/DemoScene";
-
-// export default function App() {
-//   return (
-//     <div
-//       style={{
-//         width: "100vw",
-//         height: "100vh",
-//         overflow: "hidden",
-//       }}
-//     >
-//       <DemoScene />
-//     </div>
-//   );
-// }
---
+++ apps/web/src/components/CoreScene/Shadows/App.tsx
@@ -0,0 +1,16 @@
+// import React from "react";
+// import { DemoScene } from "./components/CoreScene/Shadows/DemoScene";
+
+// export default function App() {
+//   return (
+//     <div
+//       style={{
+//         width: "100vw",
+//         height: "100vh",
+//         overflow: "hidden",
+//       }}
+//     >
+//       <DemoScene />
+//     </div>
+//   );
+// }
